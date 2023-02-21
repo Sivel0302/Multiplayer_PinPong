0c75085e74feb9b72bec2527ce76276e91cd435a
Николай <nikolai0302@yandex.ru>
2023-02-21 19:23:46 +0300

    заполнил класс PinPongBall

--- Source/Multiplayer_PinPong/MyClasses/PingPongBall.h
+++ Source/Multiplayer_PinPong/MyClasses/PingPongBall.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
 #include "GameFramework/Actor.h"
 #include "PingPongBall.generated.h"
 
@@ -10,6 +11,18 @@ UCLASS()
 class MULTIPLAYER_PINPONG_API APingPongBall : public AActor
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	USphereComponent* BodyCollision;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* BodyMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ball params")
+	float MoveSpeed = 100;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ball params")
+	UParticleSystem* HitEffect;
+	UPROPERTY(Replicated)
+	bool IsMoving = true;
 	
 public:	
 	// Sets default values for this actor's properties
@@ -19,8 +32,23 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_Move(float DeltaTime);
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_StartMove();
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_StopMove();
+	UFUNCTION(NetMulticast, Unreliable)
+	void Multicast_HitEffect();
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	void StartMove();
+	UFUNCTION(BlueprintCallable)
+	void StopMove();
+	virtual void GetLifetimeReplicatedProps(TArray< class FLifetimeProperty > &OutLifetimeProps) const;
 
 };
